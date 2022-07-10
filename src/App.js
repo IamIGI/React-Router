ff6667cb840af5fc3bc43fe4b250b05bf6dc1ba0
i.kłusek <J6ZkqwVUn>
2022-07-10 23:28:06 +0200

    useContext Api hook

--- src/App.js
+++ src/App.js
@@ -7,118 +7,24 @@ import EditPost from './EditPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
-import api from './api/posts';
-import useWindowSize from './hooks/useWindowSize';
-import useAxiosFetch from './hooks/useAxiosFetch';
+import { Route, Routes } from 'react-router-dom';
+import { DataProvider } from './context/DataContext';
 
 function App() {
-    const [posts, setPosts] = useState([]);
-    const [search, setSearch] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-    const [postTitle, setPostTitle] = useState('');
-    const [postBody, setPostBody] = useState('');
-    const [editTitle, setEditTitle] = useState('');
-    const [editBody, setEditBody] = useState('');
-    const history = useNavigate(); //have to be inside "BrowserRouter"
-    const { width } = useWindowSize();
-
-    const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
-    useEffect(() => {
-        setPosts(data);
-    }, [data]);
-
-    useEffect(() => {
-        const filteredResults = posts.filter(
-            (post) =>
-                post.title.toLowerCase().includes(search.toLowerCase()) ||
-                post.body.toLowerCase().includes(search.toLowerCase())
-        );
-        setSearchResults(filteredResults.reverse());
-    }, [posts, search]);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-        const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-        const newPost = { id, title: postTitle, datetime, body: postBody };
-        try {
-            const response = await api.post('/posts', newPost);
-            const allPosts = [...posts, response.data];
-            setPosts(allPosts);
-            setPostTitle('');
-            setPostBody('');
-            history('/');
-        } catch (err) {}
-    };
-
-    const handleEdit = async (id) => {
-        const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-        const updatedPost = { id, title: editTitle, datetime, body: editBody };
-        try {
-            const response = await api.put(`/posts/${id}`, updatedPost);
-            setPosts(posts.map((post) => (post.id === id ? { ...response.data } : post)));
-            setEditTitle('');
-            setEditBody('');
-            history('/');
-        } catch (err) {
-            console.log(`Error: ${err.message}`);
-        }
-    };
-
-    const handleDelete = async (id) => {
-        try {
-            await api.delete(`/posts/${id}`);
-            const postsList = posts.filter((post) => post.id !== id);
-            setPosts(postsList);
-            history('/'); //serve the component which is listed to "/" url
-        } catch (err) {
-            console.log(`Error: ${err.message}`);
-        }
-    };
-
     return (
         <div className="App">
-            <Header title="React JS Blog" width={width} />
-            <Nav search={search} setSearch={setSearch} /> {/* Link and Route have to be in the same BrowserRouter  */}
-            <Routes>
-                <Route
-                    exact
-                    path="/"
-                    element={<Home posts={searchResults} fetchError={fetchError} isLoading={isLoading} />}
-                />
-                <Route
-                    exact
-                    path="/post"
-                    element={
-                        <NewPost
-                            handleSubmit={handleSubmit}
-                            postTitle={postTitle}
-                            setPostTitle={setPostTitle}
-                            postBody={postBody}
-                            setPostBody={setPostBody}
-                        />
-                    }
-                />
-                <Route
-                    path="/edit/:id"
-                    element={
-                        <EditPost
-                            posts={posts}
-                            handleEdit={handleEdit}
-                            editTitle={editTitle}
-                            setEditTitle={setEditTitle}
-                            editBody={editBody}
-                            setEditBody={setEditBody}
-                        />
-                    }
-                />
-                <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Missing />} />
-            </Routes>
+            <Header title="React JS Blog" />
+            <DataProvider>
+                <Nav /> {/* Link and Route have to be in the same BrowserRouter (BrowserRouter is in the index.js)  */}
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/post" element={<NewPost />} />
+                    <Route path="/edit/:id" element={<EditPost />} />
+                    <Route path="/post/:id" element={<PostPage />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<Missing />} />
+                </Routes>
+            </DataProvider>
             <Footer />
         </div>
     );
